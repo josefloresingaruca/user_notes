f56835f1be0d5f54b429e76e34d6006cfcb4454e
josefloresingaruca <josefloresingaruca@gmail.com>
2022-10-02 02:15:14 -0500

    Creación de service, repositori, controller

--- src/main/java/com/utp/notasusuario/controllers/UsuarioController.java
+++ src/main/java/com/utp/notasusuario/controllers/UsuarioController.java
@@ -1,5 +1,10 @@
 package com.utp.notasusuario.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/usuarios")
 public class UsuarioController {
 
 }
